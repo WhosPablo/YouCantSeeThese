4974958e6d5bf19d8d19921eca87fa9136267bba
Pablo Arango <pabloaran17@gmail.com>
2015-10-24 13:21:38 -0400

    user info

--- ProcrastinationBeGone/bg.js
+++ ProcrastinationBeGone/bg.js
@@ -1,9 +1,13 @@
-var user = {};
+Parse.initialize("Xcat16hMq0jy4bEDtdzRQcDauxwTiu6Y7mN2s8By", "gtfBeoPKCkCGzbspbmfCVxrJ2dQjh7FQhxGZRI3c");
+
 var parser = document.createElement('a');
 var lastUrl = null;
 var lastTime = null;
+var currUser = Parse.User.current();
 
-Parse.initialize("Xcat16hMq0jy4bEDtdzRQcDauxwTiu6Y7mN2s8By", "gtfBeoPKCkCGzbspbmfCVxrJ2dQjh7FQhxGZRI3c");
+chrome.browserAction.onClicked.addListener(function(activeTab){
+    window.open("index.html");
+});
 
 function getRandomToken() {
     // E.g. 8 * 32 = 256 bits token
@@ -17,132 +21,144 @@ function getRandomToken() {
     return hex;
 }
 
-chrome.browserAction.onClicked.addListener(function(activeTab){
-    window.open("index.html");
-});
 
-//chrome.storage.sync.get(['userId', 'userPass'], function(o) {
-//    user = o;
-//    if(user.userId){
-//        logIn(user.userId,user.userPass);
-//    } else {
-//        var userId = getRandomToken();
-//        var userPass = getRandomToken();
-//
-//
-//        chrome.storage.sync.set({userId: userId, userPass:userPass}, function() {
-//            signUp(userId, userPass);
-//        });
-//    }
-//
-//    function logIn(userId, userPass) {
-//        Parse.User.logIn(userId, userPass, {
-//            success: function(user) {
-//                console.log("Log In Success");
-//            },
-//            error: function(pUser, error) {
-//                // The login failed. Check error to see why.
-//                console.log("Log In Error: " + error.code + " " + error.message);
-//            }
-//        });
-//    }
-//
-//    function signUp(userId, userPass) {
-//
-//        var parseUser = new Parse.User();
-//        parseUser.set("username", userId);
-//        parseUser.set("password", userPass);
-//        parseUser.signUp(null, {
-//            success: function(user) {
-//                console.log("Sign Up Success");
-//            },
-//            error: function(pUser, error) {
-//                // Show the error message somewhere and let the user try again.
-//                console.log("Sign Up Error: " + error.code + " " + error.message);
-//            }
-//        });
-//    }
-//});
+if(currUser){
+    main();
+} else {
+    chrome.storage.sync.get(['userId', 'userPass'], function(result) {
+        if(result.userId){
+            logIn(result.userId,result.userPass);
+        } else {
+            var userId = getRandomToken();
+            var userPass = getRandomToken();
+            chrome.storage.sync.set({userId: userId, userPass:userPass}, function() {
+                signUp(userId, userPass);
+            });
+        }
 
-chrome.storage.onChanged.addListener(function(changes) {
+        function logIn(userId, userPass) {
+            Parse.User.logIn(userId, userPass, {
+                success: function(user) {
+                    console.log("Log In Success");
+                    main();
+                },
+                error: function(pUser, error) {
+                    // The login failed. Check error to see why.
+                    console.log("Log In Error: " + error.code + " " + error.message);
+                }
+            });
+        }
 
-    user['userId'] = changes['userId'].newValue;
-    user['userPass'] = changes['userPass'].newValue;
-});
+        function signUp(userId, userPass) {
 
-function log(url, title){
-    parser.href = url;
-    var hostname = parser.hostname;
-    if(lastUrl !== url){
-        updateUrl(hostname, title)
-	}
-	var blockedSite = Parse.Object.extend("BlockedSite");
-	var query = new Parse.Query(blockedSite);
-	query.find({
-		success: function (results) {
-			for (var i = 0; i < results.length; i++) {
-				var object = results[i];
-				if (object.get("hostname")=== hostname) {
-					alert("you're supposed to be productive!!");
-					console.log("matched ", hostname);
-		
-				}else {
-					console.log("didn't match ", hostname, object.get("hostname"));
-				}
-			}
-		},
-		error: function (error){
-			console.log("Error: " + error.code + " " + error.message);
-		}
-	});
-
-}
-
-function updateUrl(url, title){
-
-
-    var Site = Parse.Object.extend("Site");
-
-    var query = new Parse.Query(Site);
-
-    query.equalTo("url",url );
-
-    query.first({
-        success: function(object) {
-            if(object) {
-                console.log("Successfully retrieved " + object);
-                var minDiff = Date.now() - object.get("lastAccessed");
-                //minDiff = Math.round(((minDiff % 86400000) % 3600000) / 60000);
-
-                console.log(minDiff+object.get("timeSpent"), object.get("timeSpent"));
-                object.set("timeSpent", minDiff+object.get("timeSpent"));
-                object.save();
-                lastUrl = url;
-                lastTime = Date.now();
-            } else {
-                var site1 = new Site();
-                site1.save({
-                    url: url,
-                    timeSpent: 0,
-                    lastAccessed: Date.now(),
-                    title: title
-                }).then(function(object) {
-                    console.log("yay! it worked", url);
-                });
-            }
-
-        },
-        error: function(error) {
-            console.log("Error: " + error.code + " " + error.message);
+            var parseUser = new Parse.User();
+            parseUser.set("username", userId);
+            parseUser.set("password", userPass);
+            parseUser.signUp(null, {
+                success: function(user) {
+                    console.log("Sign Up Success");
+                    main();
+                },
+                error: function(pUser, error) {
+                    // Show the error message somewhere and let the user try again.
+                    console.log("Sign Up Error: " + error.code + " " + error.message);
+                }
+            });
         }
     });
-
-
 }
 
-chrome.tabs.onActivated.addListener(function (activeInfo) {
-    chrome.tabs.get(activeInfo.tabId, function(tab) {
-        if (tab.status === "complete" && tab.active) {
+function main(){
+
+    var username = currUser.get('username');
+
+    function log(url, title){
+        parser.href = url;
+        var hostname = parser.hostname;
+        if(lastUrl !== url){
+            updateUrl(hostname, title)
+        }
+
+        var blockedSite = Parse.Object.extend("BlockedSite");
+        var query = new Parse.Query(blockedSite);
+        query.find({
+            success: function (results) {
+                for (var i = 0; i < results.length; i++) {
+                    var object = results[i];
+                    if (object.get("hostname")=== hostname) {
+                        alert("you're supposed to be productive!!");
+                        console.log("matched ", hostname);
+
+                    }else {
+                        console.log("didn't match ", hostname, object.get("hostname"));
+                    }
+                }
+            },
+            error: function (error){
+                console.log("Error: " + error.code + " " + error.message);
+            }
+        });
+
+
+    }
+
+    function updateUrl(url, title){
+
+
+        var Site = Parse.Object.extend("Site");
+
+        var query = new Parse.Query(Site);
+
+        query.equalTo("url",url );
+
+        query.first({
+            success: function(object) {
+                if(object) {
+                    console.log("Successfully retrieved " + object);
+                    var minDiff = Date.now() - object.get("lastAccessed");
+                    //minDiff = Math.round(((minDiff % 86400000) % 3600000) / 60000);
+
+                    console.log(minDiff+object.get("timeSpent"), object.get("timeSpent"));
+                    object.set("timeSpent", minDiff+object.get("timeSpent"));
+                    object.save();
+                    lastUrl = url;
+                    lastTime = Date.now();
+                } else {
+                    var site1 = new Site();
+                    site1.save({
+                        user:username,
+                        url: url,
+                        timeSpent: 0,
+                        lastAccessed: Date.now(),
+                        title: title
+                    }).then(function(object) {
+                        console.log("yay! it worked", username, url);
+                    });
+                }
+
+            },
+            error: function(error) {
+                console.log("Error: " + error.code + " " + error.message);
+            }
+        });
+
+
+    }
+
+    chrome.tabs.onActivated.addListener(function (activeInfo) {
+        chrome.tabs.get(activeInfo.tabId, function(tab) {
+            if (tab.status === "complete" && tab.active) {
+                chrome.windows.get(tab.windowId, {populate: false}, function(window) {
+                    if (window.focused) {
+                        log(tab.url, tab.title || null);
+                    }
+                });
+            }
+        });
+    });
+
+    chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+        if (changeInfo.status === "complete" && tab.active) {
             chrome.windows.get(tab.windowId, {populate: false}, function(window) {
                 if (window.focused) {
                     log(tab.url, tab.title || null);
@@ -150,30 +166,20 @@ chrome.tabs.onActivated.addListener(function (activeInfo) {
             });
         }
     });
-});
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (changeInfo.status === "complete" && tab.active) {
-        chrome.windows.get(tab.windowId, {populate: false}, function(window) {
-            if (window.focused) {
-                log(tab.url, tab.title || null);
-            }
-        });
-    }
-});
-
-chrome.windows.onFocusChanged.addListener(function (windowId) {
-    if (windowId == chrome.windows.WINDOW_ID_NONE) {
-        log(null, null, null);
-    } else {
-        chrome.windows.get(windowId, {populate: true}, function(window) {
-            if (window.focused) {
-                chrome.tabs.query({active: true, windowId: windowId}, function (tabs) {
-                    if (tabs[0].status === "complete") {
-                        log(tabs[0].url, tabs[0].title || null);
-                    }
-                });
-            }
-        });
-    }
-});
+    chrome.windows.onFocusChanged.addListener(function (windowId) {
+        if (windowId == chrome.windows.WINDOW_ID_NONE) {
+            log(null, null, null);
+        } else {
+            chrome.windows.get(windowId, {populate: true}, function(window) {
+                if (window.focused) {
+                    chrome.tabs.query({active: true, windowId: windowId}, function (tabs) {
+                        if (tabs[0].status === "complete") {
+                            log(tabs[0].url, tabs[0].title || null);
+                        }
+                    });
+                }
+            });
+        }
+    });
+}
--- ProcrastinationBeGone/index.js
+++ ProcrastinationBeGone/index.js
@@ -1,101 +1,151 @@
 Parse.initialize("Xcat16hMq0jy4bEDtdzRQcDauxwTiu6Y7mN2s8By", "gtfBeoPKCkCGzbspbmfCVxrJ2dQjh7FQhxGZRI3c");
 
+var currUser = Parse.User.current()
+if (currUser) {
+    main();
+} else {
+    chrome.storage.sync.get(['userId', 'userPass'], function (result) {
+        if (result.userId) {
+            logIn(result.userId, result.userPass);
+        } else {
+            var userId = getRandomToken();
+            var userPass = getRandomToken();
+            chrome.storage.sync.set({userId: userId, userPass: userPass}, function () {
+                signUp(userId, userPass);
+            });
+        }
 
-loadChart = function(){
+        function logIn(userId, userPass) {
+            Parse.User.logIn(userId, userPass, {
+                success: function (user) {
+                    console.log("Log In Success");
+                    main();
+                },
+                error: function (pUser, error) {
+                    // The login failed. Check error to see why.
+                    console.log("Log In Error: " + error.code + " " + error.message);
+                }
+            });
+        }
 
-    var Site = Parse.Object.extend("Site");
+        function signUp(userId, userPass) {
 
-    var data = [];
-
-    var query = new Parse.Query(Site);
-    query.descending("timeSpent");
-    query.limit(10);
-
-    query.find({
-        success: function (results) {
-            //alert("Successfully retrieved " + results.length + " scores.");
-            // Do something with the returned Parse.Object values
-            for (var i = 0; i < results.length; i++) {
-                var object = results[i];
-                var minDiff = object.get("timeSpent");
-                minDiff = Math.round(minDiff / 600)/100;
-                data.push([object.get("title"), minDiff]);
-                console.log(object.id + ' - ' + object.get('url'),object.get("timeSpent"));
-            }
-            drawChart(data);
-
-        },
-        error: function (error) {
-            console.log("Error: " + error.code + " " + error.message);
+            var parseUser = new Parse.User();
+            parseUser.set("username", userId);
+            parseUser.set("password", userPass);
+            parseUser.signUp(null, {
+                success: function (user) {
+                    console.log("Sign Up Success");
+                    main();
+                },
+                error: function (pUser, error) {
+                    // Show the error message somewhere and let the user try again.
+                    console.log("Sign Up Error: " + error.code + " " + error.message);
+                }
+            });
         }
     });
 }
 
-function drawChart (data ) {
-    $('#container').highcharts({
-        credits: {
-            enabled: false
-        },
-        exporting: {
-            enabled: false
-        },
-        chart: {
-            type: 'column',
-            animation: {
-                duration: 500
-            }
-        },
-        title: {
-            text: 'Time Spent on Sites'
-        },
-        xAxis: {
-            type: 'category',
-            labels: {
-                rotation: -45,
-                style: {
-                    fontSize: '13px',
-                    fontFamily: 'Verdana, sans-serif'
+
+function main() {
+
+    var username = currUser.get('username');
+
+    function loadChart() {
+
+        var data = [];
+
+        var Site = Parse.Object.extend("Site");
+        var query = new Parse.Query(Site);
+        query.equalTo('user', username);
+        query.descending("timeSpent");
+        query.limit(10);
+
+        query.find({
+            success: function (results) {
+                //alert("Successfully retrieved " + results.length + " scores.");
+                // Do something with the returned Parse.Object values
+                for (var i = 0; i < results.length; i++) {
+
+                    var object = results[i];
+                    var minDiff = object.get("timeSpent");
+                    minDiff = Math.round(minDiff / 600) / 100;
+                    data.push([object.get("title"), minDiff]);
+                    console.log(object.id + ' - ' + object.get('url'), object.get("timeSpent"));
                 }
+                drawChart(data);
+
+            },
+            error: function (error) {
+                console.log("Error: " + error.code + " " + error.message);
             }
-        },
-        yAxis: {
-            min: 0,
+        });
+    }
+
+    function drawChart(data) {
+        $('#container').highcharts({
+            credits: {
+                enabled: false
+            },
+            exporting: {
+                enabled: false
+            },
+            chart: {
+                type: 'column',
+                animation: {
+                    duration: 500
+                }
+            },
             title: {
-                text: 'Time (minutes)'
-            }
-        },
-        legend: {
-            enabled: false
-        },
-        tooltip: {
-            pointFormat: 'Time spent: <b>{point.y:.1f} minutes</b>'
-        },
-        series: [{
-            name: 'Time',
-            data: data,
-            dataLabels: {
-                enabled: true,
-                rotation: 0,
-                color: '#000000',
-                align: 'right',
-                format: '{point.y:.1f}', // one decimal
-                y: 0, // 10 pixels down from the top
-                x:-5,
-                style: {
-                    fontSize: '13px',
-                    fontFamily: 'Verdana, sans-serif'
+                text: 'Time Spent on Sites'
+            },
+            xAxis: {
+                type: 'category',
+                labels: {
+                    rotation: -45,
+                    style: {
+                        fontSize: '13px',
+                        fontFamily: 'Verdana, sans-serif'
+                    }
                 }
-            }
-        }]
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Time (minutes)'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            tooltip: {
+                pointFormat: 'Time spent: <b>{point.y:.1f} minutes</b>'
+            },
+            series: [{
+                name: 'Time',
+                data: data,
+                dataLabels: {
+                    enabled: true,
+                    rotation: 0,
+                    color: '#000000',
+                    align: 'right',
+                    format: '{point.y:.1f}', // one decimal
+                    y: 0, // 10 pixels down from the top
+                    x: -5,
+                    style: {
+                        fontSize: '13px',
+                        fontFamily: 'Verdana, sans-serif'
+                    }
+                }
+            }]
+        });
+    }
+
+    window.addEventListener('focus', function () {
+        loadChart();
     });
 }
-
-window.addEventListener('focus', function(){
-   loadChart();
-});
-
-
-//loadChart();
 
 
 
